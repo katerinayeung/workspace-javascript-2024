7522d007701892fb87f4be3e25a1f47ac007bdbc
katerinayeung <yeungk@usca.edu>
2024-10-28 13:16:09 -0400

    test

--- js/arith.js
+++ js/arith.js
@@ -2,16 +2,11 @@
 let spanE1 =document.getElementById('num1');
 let spanE2 =document.getElementById('num2');
 
-
+// element --- property
 let n1 = parseInt(spanE1.textContent);
 let n2 = parseInt(spanE2.textConten);
-
 console.log(typeof n1);
-
-// element --- property
 //var num1 = parseInt(document.getElementById('n1').textContent); 
-
-
 
 //process
 var c = n1 + n2;
